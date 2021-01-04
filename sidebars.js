dd908dbb9c9c8de53cdbae8db0219718f3f66a83
Dino Fejzagić <dino.f@live.de>
2021-01-04 16:10:35 +0100

    Make sidebar expanded by default

--- sidebars.js
+++ sidebars.js
@@ -1,14 +1,27 @@
 module.exports = {
-  docsSidebar: {
-    Welcome: ['about'],
-    Features: [
-      'features',
-      'unity-build-task',
-      'unity-get-project-version-task',
-      'unity-cmd-task',
-      'unity-activate-license-task',
-      'unity-test-task'
-    ],
-    Examples: ['examples']
-  },
+  docsSidebar: [
+    {
+      type: 'category',
+      label: 'Get Started',
+      collapsed: false,
+      items: ['about'],
+    },
+    {
+      type: 'category',
+      label: 'Features',
+      collapsed: false,
+      items: ['features',
+        'unity-build-task',
+        'unity-get-project-version-task',
+        'unity-cmd-task',
+        'unity-activate-license-task',
+        'unity-test-task'],
+    },
+    {
+      type: 'category',
+      label: 'Examples',
+      collapsed: false,
+      items: ['examples'],
+    }
+  ],
 };
