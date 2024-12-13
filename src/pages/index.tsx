6d1c2c6fec26b90564ede9168f5cda4199c86cc4
Dino Fejzagić <dino.f@live.de>
2024-12-13 22:00:40 +0100

    Set head meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import HeroSection from './home/HeroSection';
+import Head from '@docusaurus/Head';
 
 export default function Home(): JSX.Element {
   return (
     <Layout description="Save time and money by automating your Unity builds and deployments!">
       <main>
+        <Head>
+          <meta property='og:url' content='https://www.unitydevops.com/' />
+          <meta property='og:site_name' content='Unity Tools for Azure DevOps' />
+          <meta property='og:title' content='Unity Tools for Azure DevOps' />
+          <meta property='og:description' content='Free and open source. Save time and money by automating your Unity builds and deployments!' />
+          <meta property='og:type' content='website' />
+          <meta property='og:image' content='img/page-preview.png' />
+        </Head>
         <HeroSection />
       </main>
     </Layout>
